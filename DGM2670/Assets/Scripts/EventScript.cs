b8a2c92eb1d162d6825bec346b1b0a51094aea27
Unknown <jonnyrogersanimation@gmail.com>
2018-08-28 12:06:09 -0600

    Created DGM2670 Folder and created Event Practice Scene

---
+++ DGM2670/Assets/Scripts/EventScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class EventScript : MonoBehaviour
+{
+    public UnityEvent Event;
+
+    private void OnMouseDown()
+    {
+        Event.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Event.Invoke();
+        print("It Worked");
+    }
+}
