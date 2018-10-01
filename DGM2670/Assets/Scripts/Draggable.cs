6882159b2ae534cd79f82b6a3a7a3fd7b417f983
Jonny Rogers <jonnyrogersanimation@gmail.com>
2018-10-01 15:50:59 -0600

    Mini Gun Model, MouseDrag Scene

---
+++ DGM2670/Assets/Scripts/Draggable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour{
+
+	private Vector3 currentPosition;
+	private Vector3 newPosition;
+	private Camera cam; 
+
+	void Start () {
+		cam = Camera.main;		
+	}
+
+	private void OnMouseDown()
+	{
+		currentPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition); 
+	}
+
+	void OnMouseDrag ()
+	{
+		newPosition = currentPosition + cam.ScreenToWorldPoint(Input.mousePosition);
+		newPosition.z = 0; 
+		transform.position = newPosition;
+	}
+}
---
+++ DGM2670/Assets/Scripts/TriggerID.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerID : MonoBehaviour
+{
+
+    public NameID ID;
+    public UnityEvent OnMatch; 
+   
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (ID == obj.GetComponent<ObjectID>().ID)
+        {
+            OnMatch.Invoke();
+            print("MATCH!");
+        }
+    }
+}
