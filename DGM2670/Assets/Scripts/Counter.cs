d0e7031727b3829c4d4631265a62a6f7fa2d6f58
Jonny Rogers <jonnyrogersanimation@gmail.com>
2018-10-08 16:43:28 -0600

    Object Counter/Instantiate

---
+++ DGM2670/Assets/Scripts/Counter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Counter : MonoBehaviour
+{
+  public UnityEvent OnCount; 
+  public float Seconds = 1; 
+  IEnumerator Start()
+  {
+    while (true)
+    {
+      OnCount.Invoke(); 
+      yield return new WaitForSeconds(Seconds);
+    }
+  }
+  
+}
---
+++ DGM2670/Assets/Scripts/InstanceObject.cs
@@ -0,0 +1,11 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu]
+public class InstanceObject : ScriptableObject {
+
+	public void Call(GameObject instance)
+	{
+		Instantiate(instance); 
+	}
+}
